310a8bcf42f500fb0fe5642122c57adc538451b6
Sammwy <sammwy.dev@gmail.com>
2026-02-22 02:00:01 -0300

    fix: script

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd server && cargo build --release && cd .. && chmod +x ./server/target/release/sammwy.com && ./target/release/sammwy-web-server
+cd server && cargo build --release && cd .. && chmod +x ./server/target/release/sammwy-web-server
